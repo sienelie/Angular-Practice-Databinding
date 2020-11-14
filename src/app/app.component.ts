3734415ab28ef1ca7f5db64cf6556932bfcfed27
caknu <chrissieugay95@gmail.com>
2020-11-14 15:08:17 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  allowNewServer = false;
-  userCreationStatus = '';
   userName: string;
-
-  constructor() {
-    
-  }
-
-  onCreateUser () {
-    this.userCreationStatus = this.userName + " was added!";
-  }
-
-  onUpdateServerName (event: Event) {
-    this.userName = (<HTMLInputElement>event.target).value;
-  }
-
+ 
 }
 
 
